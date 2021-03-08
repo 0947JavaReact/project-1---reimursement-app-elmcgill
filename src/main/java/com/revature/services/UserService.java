945f30fd785183b139d9290b4a00b6081ae86c63
elmcgill <elmcgill@iastate.edu>
2021-03-08 13:50:24 -0600

    added basic functionality to the user service class, may add more as needed

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import com.revature.dao.UserDao;
 import com.revature.exceptions.EmailAlreadyExistsException;
+import com.revature.exceptions.InvalidCredentialsException;
 import com.revature.exceptions.UsernameAlreadyExistsException;
 import com.revature.models.User;
 
@@ -18,7 +19,7 @@ public class UserService {
 		this.uDao = uDao;
 	}
 	
-	//Register a new user
+	//Register a new user, returns true or false if the use was created in the db
 	public boolean registerUser(User u) {
 		
 		if(uDao.getUserByUserName(u.getUsername()) != null) {
@@ -32,6 +33,29 @@ public class UserService {
 		}
 	}
 	
+	//Logs in a user, returns the id of the logged in user
+	public int loginUser(String username, String password) {
+		
+		User toLogin = uDao.getUserByUserName(username);
+		
+		if(toLogin == null) {
+			throw new InvalidCredentialsException("Your username or password are incorrect");
+		}
+		
+		if(!toLogin.getPassword().equals(password)) {
+			throw new InvalidCredentialsException("Your username or password are incorrect");
+		}
+		
+		return toLogin.getUserId();
+	}
 	
+	public boolean removeAccount(User u, String password) {
+		
+		if(!u.getPassword().equals(password)) {
+			throw new InvalidCredentialsException("The password you supplied is incorrect");
+		}
+		
+		return uDao.deleteUser(u.getUserId());
+	}
 	
 }
